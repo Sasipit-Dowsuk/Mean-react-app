3d933a6f28d3e29606ee3f767002bf38bbd58595
Sasipit Dowsuk <minech031@gmail.com>
2025-07-17 12:35:45 +0700

    lab02

---
+++ app/routes/Mean_lab02.tsx
@@ -0,0 +1,15 @@
+function Myprofile(){
+return(
+    <div>
+        <p className="text-2xl">Mean Profile</p>
+        <img src="/images/image.png" className="w-1/4"/>
+        <p className="text-lg">
+            Name : Sasipit Dowsuk <br/>
+            Student ID : 026730491012-3 <br/>
+
+        </p>
+    </div>
+);
+}
+
+export default Myprofile;
