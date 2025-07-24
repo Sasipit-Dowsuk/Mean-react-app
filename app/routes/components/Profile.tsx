97e745f6d341aabe717c1bdcff864f1f58d1eda7
Sasipit Dowsuk <minech031@gmail.com>
2025-07-24 11:36:42 +0700

    Lab03

---
+++ app/routes/components/Profile.tsx
@@ -0,0 +1,19 @@
+export default function Profile(){
+    return (
+<>
+    <h1 className="text-2xl text-neutral-50 text-center bg-rose-900">Profile</h1>
+    <div className="p-10 bg-red-100 rounded">
+        <img src="/images/image.png" className="rounded-full w-32 shadow-lg border-4 border-neutral mx-auto"/>
+        <h2 className="text-xl text-center text-red-900 font-bold m-5">About me</h2>
+        <p className="text-center w-1/2 mx-auto">
+            ศศิพิชญ์ ดาวสุข เกิดที่ดาวพลูโต จังหวัดคันไซ <br/>
+            Student ID : 026730491012-3 <br/>
+            คณะวิทยาศาสตร์และเทคโนโลยีสารสนเทศ<br/>
+        </p>
+    </div>
+    <div className="ps-5 pe-5 pt-3 pb-1 bg-rose-400 rounded w-16 flex justifly-center mt-5">
+        <a href="/">Back</a>
+    </div>
+</>
+    );
+}
--- app/routes/lab03.tsx
+++ app/routes/lab03.tsx
@@ -1,8 +1,31 @@
 // Filename: lab03.tsx
-import Profile form "./components/profile";
+import Profile from "./components/Profile";
+import Contact from "./components/Contact";
 
 export default function Myprofile(){
-    return(
-        <Profile />
-    );
+  return (
+    <>
+      <Profile />
+      
+        <div className="flex items-center m-5 w-1/2 mx-auto">
+        <div className="grid grid-cols-3 gap-3 mx-auto">
+        <Contact
+        label="FB Sasipit Dowsuk"
+        link="https://www.facebook.com/Mirime1230"
+        icon="/images/facebook.png"
+      />
+        <Contact
+        label="IG "
+        link="https://www.instagram.com"
+        icon="/images/instagram.png"
+      />
+        <Contact
+        label="Tiktok "
+        link="https://www.tiktok.com"
+        icon="/images/tiktok.png"
+      />
+        </div>
+    </div>
+    </>
+  );
 }
